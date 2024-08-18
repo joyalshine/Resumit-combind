f6c2f5e50de6a9b61a9beaa873cde6eaaef18e52
RonnieArnab <as920037.arnabghosh@gmail.com>
2024-08-18 16:14:55 +0530

    edit resume issue fixed - I

--- frontend/src/pages/dashboard/components/AddResume.jsx
+++ frontend/src/pages/dashboard/components/AddResume.jsx
@@ -13,6 +13,7 @@ import { Input } from "@/components/ui/input";
 import { useNavigate } from "react-router-dom";
 import useGetResume from "@/useHooks/useResume";
 import { toast } from "@/components/ui/use-toast";
+import { WEBSITE_URL } from "@/assets/dataAssets";
 function AddResume() {
   const [openDialog, setOpenDialog] = useState(false);
   const [isLoading, setLoading] = useState(false);
@@ -29,7 +30,7 @@ function AddResume() {
       console.log(status, data);
 
       if (status == "success") {
-        navigate(`resume/${data._id}/edit`);
+        navigate(`${WEBSITE_URL}resume/${data._id}/edit`);
       }
     } catch (err) {
       console.log(err);
@@ -44,8 +45,8 @@ function AddResume() {
         type="button"
         onClick={() => setOpenDialog(true)}
         class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2">
-        Resume{" "}
-      </button>
+        Resume       
+      </button>
 
       <Dialog open={openDialog} onOpenChange={setOpenDialog}>
         <DialogContent className="sm:max-w-[425px]">
