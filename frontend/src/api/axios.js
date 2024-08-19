ddba207dc501cce7e6999e8c7f06c5317f77def3
Joyal Shine <101260206+joyalshine@users.noreply.github.com>
2024-08-20 01:33:56 +0530

    API url changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8000/api",
-  baseURL: "https://resume-builder-app-1.onrender.com",
+  baseURL: "https://resumit-combined.onrender.com",
 });
